b913644d9686940e666c770175425e6a63833c41
janavarroperera <janavarroperera@gmail.com>
2026-09-20 16:09:29 +0100

    Implement order crossover logic in solve.py and add corresponding documentation

--- Semana 1/S1a_ Manejo de arrays/joseangel/solve.py
+++ Semana 1/S1a_ Manejo de arrays/joseangel/solve.py
@@ -20,5 +20,52 @@ def solve(input_list):
         - No imprimir resultados dentro de esta función.
         - Devolver un número entero.
     """
-    pass
+
+    def limpiar_lista(lista_a_limpiar):
+        lista_final = []
+
+        lista_a_limpiar = lista_a_limpiar.split(",")
+
+        for numero in lista_a_limpiar:
+            lista_final.append(int(numero))
+
+        return lista_final
+
+
+    padre = input_list[0][0:-1]
+    madre = input_list[1][0:-1]
+
+    padre = limpiar_lista(padre)
+    madre = limpiar_lista(madre)
+
+    lower_bound = int(input_list[2][0:-1])
+    upper_bound = int(input_list[3])
+
+    hijo = [None] * len(padre)
+    hijo[lower_bound:upper_bound] = padre[lower_bound:upper_bound] 
+
+    i = upper_bound
+    j = upper_bound 
+    while None in hijo:
+        if hijo[i] is None:
+            if madre[j] not in hijo:
+                hijo[i] = madre[j]
+                if i == len(hijo) - 1:
+                    i = 0
+                else:
+                    i += 1
+
+            if j == len(madre) -1:
+                j = 0
+            else:
+                j += 1
+
+
+        else: 
+            if i == len(hijo) - 1:
+                i = 0
+            else:
+                i += 1
+
+    return hijo
 
